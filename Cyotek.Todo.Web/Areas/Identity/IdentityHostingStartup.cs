dfae5f08fcf8631dd9e2096843f04a33e74551e5
Richard Moss <richard.moss@cyotek.com>
2018-08-08 17:50:46 +0100

    Added identity scaffolding

---
+++ Cyotek.Todo.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Cyotek.Todo.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Cyotek.Todo.Areas.Identity.IdentityHostingStartup))]
+namespace Cyotek.Todo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
